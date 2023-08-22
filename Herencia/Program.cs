0eea0d2357f632b903d6e5ee813948ce612aff74
BruH_C <101187609+HurBC@users.noreply.github.com>
2023-08-22 01:06:09 -0400

    uwu

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿interface IMamiferosTerrestres
+{
+
+    int numPatas();
+
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main( string[] args )
     {
 
-        Caballo Spirit = new Caballo();
+        Caballo Spirit = new Caballo("Spirit");
 
-        Humano Franco = new Humano();
+        Humano Tyler = new Humano("Tyler");
 
-        Gorila Copito = new Gorila();
+        Gorila Copito = new Gorila("Gorila");
+
+        Ballena Wally = new Ballena("Wally");
+
+        Mamiferos[] almacen = new Mamiferos[4];
+
+        almacen[0] = Spirit;
+
+        almacen[1] = Tyler;
+
+        almacen[2] = Copito;
+
+        almacen[3] = Wally;
+
+        Wally.Nadar();
+
+        Console.WriteLine("Numero de patas: {0}", Spirit.numPatas());
+
+        for (int i = 0; i < almacen.Length; i++)
+        {
+
+            almacen[i].Pensar();
+
+        }
 
     }
 }
@@ -17,41 +45,125 @@ class Program
 class Mamiferos
 {
 
-    public void Respirar()
+    public Mamiferos( string name )
     {
-        Console.WriteLine("Puedo respirar xd");
+
+        _nombreSerVivo = name;
+
+    }
+
+    protected void Respirar()
+    {
+
+        Console.WriteLine( "Puedo respirar xd" );
+
+    }
+
+    public virtual void Pensar()
+    {
+
+        Console.WriteLine( "Pensamiento basico instintitvo" );
+
     }
 
     public void CuidarCrias()
     {
-        Console.WriteLine("Cuido a mis crias xd");
+
+        Console.WriteLine( "Cuido a mis crias xd" );
+
     }
+
+    public void GetName()
+    {
+
+        Console.WriteLine( "El nombre es: {0}", _nombreSerVivo );
+
+    }
+
+    private string _nombreSerVivo;
 
 }
 
-class Caballo : Mamiferos
+class Ballena : Mamiferos
 {
-    public void Galopar()
+    public Ballena( string name ) : base( name )
     {
-        Console.WriteLine("Galopo xd");
+
     }
 
+    public void Nadar()
+    {
+
+        Console.WriteLine( "Soy capaz de nadar" );
+
+    }
+}
+
+class Caballo : Mamiferos, IMamiferosTerrestres
+{
+
+    public Caballo( string name ) : base( name )
+    {
+
+    }
+
+    public void Galopar()
+    {
+
+        Console.WriteLine( "Galopo xd" );
+
+        Respirar();
+
+    }
+
+    public int numPatas()
+    {
+
+        return 4;
+
+    }
 }
 
 class Humano : Mamiferos
 {
-
-    public void Pensar()
+    public Humano( string name ) : base( name )
     {
-        Console.WriteLine("Estoy pensando xd");
+
     }
 
+    public override void Pensar()
+    {
+
+        Console.WriteLine( "Estoy pensando xd" );
+
+    }
 }
 
-class Gorila : Mamiferos
+class Gorila : Mamiferos, IMamiferosTerrestres
 {
+    public Gorila( string name ) : base( name )
+    {
+
+    }
+
     public void Trepar()
     {
-        Console.WriteLine("Puedo trepar xd");
+
+        Console.WriteLine( "Puedo trepar xd" );
+
+    }
+
+    public override void Pensar()
+    {
+
+        Console.WriteLine( "Pensar en gorila" );
+
+    }
+
+    public int numPatas()
+    {
+
+        return 2;
+
     }
 }
