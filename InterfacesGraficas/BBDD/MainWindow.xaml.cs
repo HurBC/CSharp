9f1efc693a582d69abbd99e182b258296d4c5ea7
BruH_C <101187609+HurBC@users.noreply.github.com>
2023-09-03 23:39:49 -0300

    DELETE

--- InterfacesGraficas/BBDD/MainWindow.xaml.cs
+++ InterfacesGraficas/BBDD/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Data;
 
 namespace BBDD
 {
@@ -27,36 +17,156 @@ namespace BBDD
         {
             InitializeComponent();
 
+            // Inicializa la conexión a la base de datos recuperando la cadena de conexión desde el archivo de configuración de la aplicación.
             string ConnectionBD = ConfigurationManager.ConnectionStrings["BBDD.Properties.Settings.GestionPedidosConnectionString"].ConnectionString;
 
-            _connection = new SqlConnection(ConnectionBD);
+            // Crea un objeto SqlConnection y le pasa la cadena de conexión a la base de datos como parámetro.
+            _connection = new SqlConnection( ConnectionBD );
 
+            // Llama al método para mostrar los nombres de los clientes.
             MostrarNombres();
+
+            All_Products();
 
         }
 
         private void MostrarNombres()
         {
 
-            string Query = "SELECT * FROM Cliente";
-
-            SqlDataAdapter sqlData = new SqlDataAdapter(Query, _connection);
-
-            using (sqlData)
+            try
             {
+                // Consulta SQL para recuperar datos de la tabla "Cliente".
+                string Query = "SELECT * FROM Cliente";
 
-                DataTable data = new DataTable();
+                // Utiliza un SqlDataAdapter para ejecutar la consulta y recuperar datos en un DataTable.
+                SqlDataAdapter sqlData = new SqlDataAdapter(Query, _connection);
 
-                sqlData.Fill(data);
+                using (sqlData)
+                {
 
-                ClientesList.DisplayMemberPath = "nombre";
-                ClientesList.SelectedValuePath = "Id";
-                ClientesList.ItemsSource = data.DefaultView;
+                    // Crea un DataTable para almacenar los datos recuperados.
+                    DataTable data = new DataTable();
 
+                    // Llena el DataTable con los resultados de la consulta.
+                    sqlData.Fill( data );
+
+                    // Configura la vinculación de datos para un elemento de interfaz de usuario (ClientesList) para mostrar los nombres de los clientes.
+                    ClientesList.DisplayMemberPath = "nombre";
+                    ClientesList.SelectedValuePath = "Id";
+                    ClientesList.ItemsSource = data.DefaultView;
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show( ex.ToString() );
             }
 
         }
 
+        private void MostrarPedidos()
+        {
+            try
+            {
+
+                // Consulta SQL para recuperar datos de la tabla "Pedido" al unirlo con la tabla "Cliente" usando la relación entre sus Id.
+                string Query = "SELECT * FROM Pedido p INNER JOIN Cliente c ON c.Id = p.cCliente" +
+                " WHERE c.Id=@ClienteID";
+
+                // Crea un objeto SqlCommand con la consulta y la conexión establecida.
+                SqlCommand command = new SqlCommand(Query, _connection);
+
+                // Crea un SqlDataAdapter para ejecutar el comando SQL.
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+
+                using (sqlData)
+                {
+
+                    // Agrega un parámetro al comando para filtrar por el ID del cliente seleccionado en la lista.
+                    command.Parameters.AddWithValue( "@ClienteID", ClientesList.SelectedValue );
+
+                    // Crea un DataTable para almacenar los datos recuperados.
+                    DataTable data = new DataTable();
+
+                    // Llena el DataTable con los resultados de la consulta.
+                    sqlData.Fill( data );
+
+                    // Configura la vinculación de datos para un elemento de interfaz de usuario (ProductosList) para mostrar la fecha de pedido.
+                    FechaPedidosList.DisplayMemberPath = "fechaPedido";
+                    FechaPedidosList.SelectedValuePath = "Id";
+                    FechaPedidosList.ItemsSource = data.DefaultView;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.ToString() );
+            }
+        }
+
+        private void All_Products()
+        {
+            try
+            {
+
+                // Consulta SQL para recuperar datos de la tabla "Pedido" al unirlo con la tabla "Cliente" usando la relación entre sus Id.
+                string Query = "SELECT *, CONCAT(P.cCliente, ' ', P.fechaPedido, ' ', P.formaPago) as 'Info_Pedido'" +
+                   "FROM Pedido P";
+
+                SqlDataAdapter sqlData = new SqlDataAdapter(Query, _connection);
+
+                using (sqlData)
+                {
+                    DataTable data = new DataTable();
+
+                    // Llena el DataTable con los resultados de la consulta.
+                    sqlData.Fill( data );
+
+                    // Configura cómo se muestra la información en la lista PedidosList.
+                    PedidosList.DisplayMemberPath = "Info_Pedido"; // Muestra "Info_Pedido".
+                    PedidosList.SelectedValuePath = "Id"; // Usa "Id" como valor seleccionado.
+                    PedidosList.ItemsSource = data.DefaultView; // Establece los datos de origen como la vista predeterminada de la tabla data.
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.ToString() );
+            }
+
+        }
+
+        private void ClientesList_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+            // Llama al método para mostrar los pedidos para el cliente seleccionado cuando cambia la selección.
+            MostrarPedidos();
+        }
+
         SqlConnection _connection;
+
+        private void Button_Click( object sender, RoutedEventArgs e )
+        {
+
+            // Consulta SQL para eliminar un pedido por su Id.
+            string Query = "DELETE FROM Pedido WHERE Id = @PedidoID";
+
+            // Crea un comando SQL con la consulta y la conexión proporcionadas.
+            SqlCommand command = new SqlCommand(Query, _connection);
+
+            // Abre la conexión a la base de datos.
+            _connection.Open();
+
+            // Establece el valor del parámetro "@PedidoID" con el valor seleccionado de la lista PedidosList.
+            command.Parameters.AddWithValue( "@PedidoID", PedidosList.SelectedValue );
+
+            // Ejecuta la consulta SQL para eliminar el pedido.
+            command.ExecuteNonQuery();
+
+            // Llama al método All_Products para actualizar la lista de pedidos después de la eliminación.
+            All_Products();
+
+            // Cierra la conexión a la base de datos después de realizar la operación.
+            _connection.Close();
+
+        }
     }
 }
