73df617a9712a7c819fc9dfd9c7ced285f683631
BruH_C <101187609+HurBC@users.noreply.github.com>
2023-10-02 01:50:36 -0300

    Arrays in CodeWithSenko

--- Consola/CodeWithSenkoEX/Program.cs
+++ Consola/CodeWithSenkoEX/Program.cs
@@ -4,11 +4,30 @@
     {
         static void Main( string[] args )
         {
-            Random random = new Random();
 
-            int randomNumber, value;
+            int[,] ints = { {1, 2, 3}, { 4, 5, 6}, { 7, 8, 9 } };
+            int[][] ints2 = { new int[3] {1,2,3}, new int[3] {4,5,6}, new int[3] {7,8,9} };
 
-            randomNumber = random.Next(1, 101);
+            // multidimensional Arrays
+            for ( int i = 0;  i < ints.GetLength(0); i++ )
+            {
+                for ( int j = 0; j < ints.GetLength(1); j++ )
+                {
+                    Console.Write( ints[i, j] + " " );
+                }
+                Console.WriteLine();
+            }
+
+            // Jagged Arrays
+            foreach ( var i in ints2 )
+            {
+                foreach (var j in i )
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
