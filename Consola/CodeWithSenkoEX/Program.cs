b3f4a2286348db21c3c54d761d43f1c06b4ec73c
Franco carrasco <franco.carrasco.obreque@alumnos.sip.cl>
2023-10-02 15:27:36 -0300

    asdas

--- Consola/CodeWithSenkoEX/Program.cs
+++ Consola/CodeWithSenkoEX/Program.cs
@@ -2,32 +2,19 @@
 {
     internal class Program
     {
+
+        static void sayHello(string name = "invited" )
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
         static void Main( string[] args )
         {
 
-            int[,] ints = { {1, 2, 3}, { 4, 5, 6}, { 7, 8, 9 } };
-            int[][] ints2 = { new int[3] {1,2,3}, new int[3] {4,5,6}, new int[3] {7,8,9} };
-
-            // multidimensional Arrays
-            for ( int i = 0;  i < ints.GetLength(0); i++ )
-            {
-                for ( int j = 0; j < ints.GetLength(1); j++ )
-                {
-                    Console.Write( ints[i, j] + " " );
-                }
-                Console.WriteLine();
-            }
-
-            // Jagged Arrays
-            foreach ( var i in ints2 )
-            {
-                foreach (var j in i )
-                {
-                    Console.Write(j + " ");
-                }
-                Console.WriteLine();
-            }
+            sayHello();
+            sayHello( "Senko" );
 
         }
+
     }
 }
